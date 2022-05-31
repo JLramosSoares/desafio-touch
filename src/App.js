cde192147ddcffa73f78d74aacae98b96bfa1503
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2022-05-31 14:28:17 -0300

    Meus arquivos do desafio-touch
    Arquivos que fiz até agora

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import React,{useState, useEffect} from 'react';
+import api from './services/api';
+
+class Clock extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {date:new Date()};
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID)
+    }
+
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render(){
+        return(
+            <>
+                {this.state.date.toLocaleTimeString()}
+            </>
+        );
+    }
+}
+
+
+function Header(){
+    return(
+        <div className="Header">
+            <h2>Casa de Cambio</h2>
+        </div>
+    );
+};
+
+function Title(){
+    return(
+        <div>
+            <h1>Casa de Cambio</h1>
+        </div>
+    );
+};
+
+
+function Table(){
+    
+    const[moedas, setMoedas] = useState();
+
+    useEffect(() => {
+        api
+            .get('/USD-BRL,CAD-BRL,EUR-BRL,BTC-BRL,BOB-BRL,CHF-BRL')
+            .then((response) => setMoedas(response.data))
+            .catch((err) => {
+                console.log('Ops! Ocorreu um erro...'+err);
+            })
+    },[]);
+    
+
+    const bids = []
+    const lows = []
+
+
+    return(
+        <div className="Table">
+            <p>Tabela de cotação - {new Date().toLocaleDateString()}, {<Clock/>}</p>
+            <table>
+                <tr>
+                    <th>Moeda</th>
+                    <th>Valor de Compra</th>
+                    <th>Valor de Venda</th>
+                </tr>
+                <tr>
+                    <td>USD</td>
+                    <td>{moedas?.USDBRL.bid}</td>
+                    <td>{moedas?.USDBRL.low}</td>
+                </tr>
+                <tr>
+                    <td>CAD</td>
+                    <td>{moedas?.CADBRL.bid}</td>
+                    <td>{moedas?.CADBRL.low}</td>
+                </tr>
+                <tr>
+                    <td>EUR</td>
+                    <td>{moedas?.EURBRL.bid}</td>
+                    <td>{moedas?.EURBRL.low}</td>
+                </tr>
+                <tr>
+                    <td>BOB</td>
+                    <td>{moedas?.BOBBRL.bid}</td>
+                    <td>{moedas?.BOBBRL.low}</td>
+                </tr>
+                <tr>
+                    <td>CHF</td>
+                    <td>{moedas?.CHFBRL.bid}</td>
+                    <td>{moedas?.CHFBRL.low}</td>
+                </tr>
+            </table>
+        </div>
+    );
+};
+
+function App(){
+    return(
+        <div className="App">
+            <Header/>
+            <Title/>
+            <Table/>
+        </div>
+    );
+};
+
+export default App;
